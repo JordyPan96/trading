d5276bf4557e0b4acb220f072069b746e8af91a5
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2025-10-07 14:28:45 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2085,8 +2085,9 @@ elif st.session_state.current_page == "Risk Calculation":
                     round((round(monthly_loss_limit, 2)+ round(monthly_actual_loss, 2)) / starting_capital * 100,
                           2)) + "%"]
             }).set_index("Metric")
-            st.markdown("<div style='margin-right: 100px;'></div>", unsafe_allow_html=True)
+
             st.table(result_df)
+            st.markdown("&nbsp;" * 20, unsafe_allow_html=True)
             next_risk = base_risk
 
         # Google Sheets integration functions

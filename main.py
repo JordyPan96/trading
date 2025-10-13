5f09c29e85b3f74cb441b6a1a94f12c14eef9e83
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2025-10-13 13:24:37 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3903,7 +3903,7 @@ elif st.session_state.current_page == "Active Opps":
                             else:
                                 st.write(f"**{time_str}** [{currency}] {event_name}")
 
-                    st.markdown("<div style='height:20px;'></div>", unsafe_allow_html=True)
+                    st.markdown("<div style='height:15px;'></div>", unsafe_allow_html=True)
     else:
         st.info("No high-impact events found.")
 

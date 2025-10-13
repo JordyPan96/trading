98cada50ededda9fd8998610ff6b159921467b33
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2025-10-13 15:26:08 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3429,7 +3429,7 @@ elif st.session_state.current_page == "Risk Calculation":
                                     # Check if Stop Loss is 0 or missing
                                     if stop_pips is None or stop_pips == 0:
                                         st.error("Cannot add order: Stop Loss is required and cannot be 0!")
-                                    elif (position_size <= 0):
+                                    elif (position_size <= 0 or final_risk <=0):
                                         st.error("Position size is required to be greater than 0")
                                     else:
                                         # Check if maximum records reached (only for new records, not updates)
@@ -3553,7 +3553,7 @@ elif st.session_state.current_page == "Risk Calculation":
                                     if stop_pips is None or stop_pips == 0:
 
                                         st.error("Cannot add order: Stop Loss is required and cannot be 0!")
-                                    elif (position_size <= 0):
+                                    elif (position_size <= 0 or final_risk <= 0):
                                         st.error("Position size is required to be greater than 0")
 
                                     else:

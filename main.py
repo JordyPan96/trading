f4c2e56a710122ac1d2c43d419f82122afaacb90
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2025-10-20 10:16:42 +1100

    Update main.py

--- main.py
+++ main.py
@@ -4025,7 +4025,7 @@ elif st.session_state.current_page == "Risk Calculation":
                             <div style="color:deepskyblue; font-weight:600;">
                                 --Entry must not exceed 33%<br>
                                 --Enter Middle of Zone if TPF last 25th Percentile<br>
-                                --Entry: {get_global('entry_model')}
+                                --Entry: {get_global('entry_model')}<br>
                             </div>
                             """, unsafe_allow_html=True)
                 elif (get_global('entry_model') == None):
@@ -4034,7 +4034,7 @@ elif st.session_state.current_page == "Risk Calculation":
                             <div style="color:deepskyblue; font-weight:600;">
                                 --Entry must not exceed 33%<br>
                                 --Enter Middle of Zone if TPF last 25th Percentile<br>
-                                --Min Length for {selected_pair} is {base_percent}%
+                                --Min Length for {selected_pair} is {base_percent}%<br>
                             </div>
                             """, unsafe_allow_html=True)
 

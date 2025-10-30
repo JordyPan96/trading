5b34a483502eb58172a4ac83ffcc83a177a81e5b
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2025-10-30 22:42:19 +1100

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ st.markdown("""
         }
 
         .stApp {
-            margin-top: 100px !important;
+            margin-top: 200px !important;
         }
 
         /* Force centered layout on mobile */
@@ -8697,4 +8697,5 @@ if st.session_state.current_page == "Entry Criteria Check":
 
     if __name__ == "__main__":
         main()
+
 

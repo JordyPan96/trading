a133490922e78b1451abf29483844fdff6727079
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2025-11-08 12:43:37 +1100

    Update main.py

--- main.py
+++ main.py
@@ -4676,7 +4676,7 @@ elif st.session_state.current_page == "Risk Calculation":
                             <div style="color:deepskyblue; font-weight:600;">
                                 --EP (Entry Precision) Line Priority Guide Based on Entry Model<br>
                                 --1_BNR_TPF/2_BNR_TPF: 5% from top of TPF -> Entry cover EOZ (+1 if not) -> Within 64 Check<br>
-                                --2_BNR: Top of Zone -> Entry cover EOZ (Must) -> Within 64 Check<br>
+                                --2_BNR: Entry cover EOZ (Must) -> Within 64 Check -> Entry at TOP of ZONE<br>
                                 --<br>
                                 --50 Zone Must be Merged Top or merged bottom (Double Zone)<br>
                             </div>
@@ -4688,7 +4688,7 @@ elif st.session_state.current_page == "Risk Calculation":
                             <div style="color:deepskyblue; font-weight:600;">
                                 --EP (Entry Precision) Line Priority Guide Based on Entry Model<br>
                                 --1_BNR_TPF/2_BNR_TPF: 5% from top of TPF -> Entry cover EOZ (+1 if not) -> Within 64 Check<br>
-                                --2_BNR: Top of Zone -> Entry cover EOZ (Must) -> Within 64 Check<br>
+                                --2_BNR: Entry cover EOZ (Must) -> Within 64 Check -> Entry at TOP of ZONE<br>
                                 --<br>
                                 --50 Zone Must be Merged Top or merged bottom (Double Zone)<br>
                             </div>
@@ -8765,6 +8765,7 @@ if st.session_state.current_page == "Entry Criteria Check":
 
     if __name__ == "__main__":
         main()
+
 
 
 

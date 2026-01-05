bd78de571ba9a1a750095f64635be652c96568af
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2026-01-06 09:31:15 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3688,7 +3688,7 @@ elif st.session_state.current_page == "Risk Calculation":
         incompatible_map_24 = {
             ">=13% (5% PullBack)": ["Cross Wave 3.5 - 4.99"],
             "11%-12.99% (3.5% PullBack)": [],
-            "3%-4.99%": ['Cross Wave', "Cross Wave 3.5 - 4.99"]
+            "3%-4.99%": []
         }
 
         incompatible_map_25 = {
@@ -9423,6 +9423,7 @@ if st.session_state.current_page == "Entry Criteria Check":
 
     if __name__ == "__main__":
         main()
+
 
 
 

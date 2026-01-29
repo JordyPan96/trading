ca6a9ac009c690e54a6f5c599fb5b3ae50d457ee
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2026-01-29 11:59:40 +1100

    Update main.py

--- main.py
+++ main.py
@@ -4265,7 +4265,7 @@ elif st.session_state.current_page == "Risk Calculation":
             available_time_frame_2 = get_available_timeframe_2(available_time_frame, trend_position)
             available_time_frame_3 = get_available_timeframe_3(available_time_frame_2, cross_fib)
             POI = st.selectbox(
-                "POI Type (If wave >4%, AUDCAD >3% use 2 Daily Fib, 1_BNR adjust based on mid Weekly K, 1_BNR_TPF adjust based on TPF position)",
+                "POI Type (If wave >4%, AUDCAD >3% use 2 Daily Fib) If 2_Daily unclear use Mid Weekly K and TPF position to adjust",
                 available_time_frame_3)
 
             HH_LL = st.selectbox("FIB drawn on Highest High (Buy)/ Lowest Low (Sell)", ['Yes', 'No'])
@@ -9514,6 +9514,7 @@ if st.session_state.current_page == "Entry Criteria Check":
 
     if __name__ == "__main__":
         main()
+
 
 
 

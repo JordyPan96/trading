901e6ab60329abcd485f706ab4379454ee73369d
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2026-04-05 11:22:36 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3505,6 +3505,7 @@ elif st.session_state.current_page == "Risk Calculation":
 
         strategies = ['1_BNR', '1_BNR_TPF', '2_BNR', '2_BNR_TPF', "No Setup"]
         shapes = ["On OMSS (With TPF)", "On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
+                  "Zone/On Left TPF + Left TPF", "Zone/On Left TPF + OMSS", "Zone/On Left TPF",
                   "Variant 2 Daily TPF", "Variant Fakeout Any TF TPF", "4H BE Scale-in", "No Pattern"]
         time_frame = ['1 Trade','2 Trades']
         _559_time = ['0', '1', '2']

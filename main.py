90fac306d92d7237f0338060bffd451b8a814e82
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2026-04-10 08:43:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5165,37 +5165,37 @@ elif st.session_state.current_page == "Risk Calculation":
                                         open_target = 5.41
                             
                         elif(context == "Trend 1+"):
-                            if(wave == "Cross Trend >=6%" or wave == "Cross Trend 4% - 5.99%" or wave == "Cross Trend <=3.99%"):
+                            if(wave == "Cross Trend 4% - 5.99%" or wave == "Cross Trend <=3.99%"):
                                 if(trend == "3%-4.99%"):
-                                    open_target = 5.41
+                                    open_target = 4.11
                                 elif(trend == "5%-6.99%"):
-                                    open_target = 5.41
+                                    open_target = 4.11
                                 elif(trend == "7%-8.99%"):
-                                    open_target = 5.41
+                                    open_target = 4.11
                                 elif(trend == "9%-10.99%"):
-                                    open_target = 5.41
+                                    open_target = 4.11
                                 elif(trend == "11%-12.99% (4% PB)"):
-                                    open_target = 5.41
+                                    open_target = 4.11
                                 elif(trend == ">=13% (5% PB, ACAD 4%)"):
-                                    open_target = 5.41
+                                    open_target = 4.11
                             elif(wave == "Wave 2+"):
                                 if(trend == "3%-4.99%"):
-                                    open_target = "Top of trend to 8.99%"
+                                    open_target = 5.41
                                 elif(trend == "5%-6.99%"):
-                                    open_target = "Top of trend to 8.99%"
+                                    open_target = 5.41
                                 elif(trend == "7%-8.99%"):
-                                    open_target = 5.41
+                                    open_target = 7.41
                                 elif(trend == "9%-10.99%"):
-                                    open_target = 5.41
+                                    open_target = 10.41
                                 elif(trend == "11%-12.99% (4% PB)"):
                                     open_target = 5.41
                                 elif(trend == ">=13% (5% PB, ACAD 4%)"):
                                     open_target = 5.41
-                            elif(wave == "Wave 1"):
+                            elif(wave == "Wave 1" or wave == "Cross Trend >=6%" or ):
                                 if(trend == "3%-4.99%"):
-                                    open_target = "Top of trend to 8.99%"
+                                    open_target = 7.41
                                 elif(trend == "5%-6.99%"):
-                                    open_target = "Top of trend to 8.99%"
+                                    open_target = 10.41
                                 elif(trend == "7%-8.99%"):
                                     open_target = "Top of trend to 9%"
                                 elif(trend == "9%-10.99%"):

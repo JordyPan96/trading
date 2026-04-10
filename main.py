c783c35e5d5fc90621ff22f8e1e0bc40e5e856eb
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2026-04-10 13:35:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1258,7 +1258,7 @@ if st.session_state.current_page == "Home":
                 hh_ll_options = ["Yes", "No"]
                 new_hh_ll = st.selectbox("HH/LL", options=hh_ll_options, key="new_hh_ll")
 
-                pattern_options = ["On OMSS (With TPF)", "On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
+                pattern_options = ["On OMSS (With TPF)", "Zone/On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
                   "Zone/On Left TPF + Left TPF", "Zone/On Left TPF + OMSS", "Zone/On Left TPF",
                   "Variant 2 Daily TPF", "Variant Fakeout Any TF TPF", "4H BE Scale-in", "No Pattern"]
                 new_Pattern = st.selectbox("Pattern", options=pattern_options, key="new_Pattern")
@@ -3505,7 +3505,7 @@ elif st.session_state.current_page == "Risk Calculation":
         minors = ["GBPAUD", "EURAUD", "GBPJPY", "EURJPY", "AUDJPY"]
 
         strategies = ['1_BNR', '1_BNR_TPF', '2_BNR', '2_BNR_TPF', "No Setup"]
-        shapes = ["On OMSS (With TPF)", "On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
+        shapes = ["On OMSS (With TPF)", "Zone/On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
                   "Zone/On Left TPF + Left TPF", "Zone/On Left TPF + OMSS", "Zone/On Left TPF",
                   "Variant 2 Daily TPF", "Variant Fakeout Any TF TPF", "4H BE Scale-in", "No Pattern"]
         time_frame = ['1 Trade','2 Trades']
@@ -3653,11 +3653,11 @@ elif st.session_state.current_page == "Risk Calculation":
         incompatible_map_17 = {
             '1_BNR': ["Zone/On Left TPF + Left TPF", "Zone/On Left TPF + OMSS", "Zone/On Left TPF",
                       "Variant 2 Daily TPF", "Variant Fakeout Any TF TPF","4H BE Scale-in"],
-            '1_BNR_TPF': ["On OMSS (With TPF)", "On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
+            '1_BNR_TPF': ["On OMSS (With TPF)", "Zone/On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
                           "4H BE Scale-in", "Variant 2 Daily TPF", "Variant Fakeout Any TF TPF"],
             '2_BNR': ["Zone/On Left TPF + Left TPF", "Zone/On Left TPF + OMSS", "Zone/On Left TPF", 
                       "Variant 2 Daily TPF", "Variant Fakeout Any TF TPF"],
-            '2_BNR_TPF': ["On OMSS (With TPF)", "On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
+            '2_BNR_TPF': ["On OMSS (With TPF)", "Zone/On OB (With TPF)", "Zone/On Left TPF + Left Wick", "Zone/On Right Impulse Candle Wick",
                          ]
         }
 

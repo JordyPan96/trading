9dc860d59b066afe2fc395ba55c0b51606197622
JordyPan96 <167163858+JordyPan96@users.noreply.github.com>
2026-05-07 13:38:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4409,7 +4409,7 @@ elif st.session_state.current_page == "Risk Calculation":
 
             
             available_wave_status = get_available_Wave_status(trend_position)
-            cross_fib = st.selectbox("Weekly Wave Status -  (Use Weekly or 2_Daily Fib, whatever makes sense)", available_wave_status)
+            cross_fib = st.selectbox("Weekly Wave Status -  (Use Weekly or 2_Daily Fib, based on Wave + %)", available_wave_status)
             available_time_frame = get_available_timeframe(selected_pair)
             available_time_frame_2 = get_available_timeframe_2(available_time_frame, trend_position)
             available_time_frame_3 = get_available_timeframe_3(available_time_frame_2, cross_fib)
